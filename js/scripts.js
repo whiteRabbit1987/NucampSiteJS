e192817706328ae52f1f8bee4d1781c4e1ece512
whiteRabbit87 <davidstephenmoseley1987@gmail.com>
2024-08-01 12:23:42 -0500

    Custom Javascript Part2

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,25 @@
-console.log('javascript connected!');
-
 const carousel = new bootstrap.Carousel('#homeCarousel', {
-    interval: 2000,
+    interval: 5000,
     pause: false
 })
 
-// when the pause button is clicked, pause the carousel
-const carouselPause = document.getElementById('carouselPause');
-carouselPause.addEventListener('click', function() {
-    console.log('pausing the carousel');
-    carousel.pause();
+const carouselButton = document.getElementById('carouselButton');
+const faIcon = document.getElementById('faButton');
+
+carouselButton.addEventListener('click', function () {
+    if(faIcon.classList.contains('fa-pause')) {
+        faIcon.classList.remove('fa-pause');
+        faIcon.classList.add('fa-play');
+        carouselButton.classList.remove('btn-danger');
+        carouselButton.classList.add('btn-info');
+        carousel.pause();
+    } else {
+        faIcon.classList.remove('fa-play');
+        faIcon.classList.add('fa-pause');
+        carouselButton.classList.remove('btn-info');
+        carouselButton.classList.add('btn-danger');
+        carousel.cycle();
+    }
 })
 
-// when the play button is clicked, begin cycling through the images
-const carouselPlay = document.getElementById('carouselPlay');
-carouselPlay.addEventListener('click', function() {
-    console.log('cycle the carousel');
-    carousel.cycle();
-})
+
